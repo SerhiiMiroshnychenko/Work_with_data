3cefda93b1ffbd660419b079abcd0db57294df72
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-31 11:54:29 +0200

    sitka_highs.py was changed. Printed maximum temperatures.

--- sitka_highs.py
+++ sitka_highs.py
@@ -6,5 +6,15 @@ with open(filename) as f:
     header_row = next(reader)  # Функція next() 'наступний' повертає наступний рядок у файлі
     # ^ Викликаємо лише один раз, щоб отримати перший рядок файлу, що містить файлові заголовки
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    for index, column_header in enumerate(header_row):  # Функція enumerate() повертає
+        # індекс кожного елемента та його значення
+        print(index, column_header)  # Виводимо індекси стовпців та іх назви
+
+    # Отримати високі температури з цього файлу
+    highs = []
+    for row in reader:  # Об'єкт reader продовжує роботу там, де зупинився останній раз
+        # Позаяк ми вже зчитали назви стовпців, то починаємо роботу з їх вмістом
+        high = int(row[5])  # Витягаємо данні з індексом [5] -> TMAX == максимальна температура
+        highs.append(high)  # Формуємо список з цих даних
+
+print(highs)
