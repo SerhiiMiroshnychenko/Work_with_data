1bd07daaf3144210ac76658ff143157cadc8a9be
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-31 11:43:08 +0200

    sitka_highs.py was changed.

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,4 +5,6 @@ with open(filename) as f:
     reader = csv.reader(f)  # Створюємо об'єкт-читач і прив'язуємо його до змінної reader
     header_row = next(reader)  # Функція next() 'наступний' повертає наступний рядок у файлі
     # ^ Викликаємо лише один раз, щоб отримати перший рядок файлу, що містить файлові заголовки
-    print(header_row)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
