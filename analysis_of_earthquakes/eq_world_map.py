92a29b6e2af365d955379a3f07ca91c7594b51e0
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-11-01 15:38:41 +0200

    A map of global earthquakes has been made.

--- analysis_of_earthquakes/eq_world_map.py
+++ analysis_of_earthquakes/eq_world_map.py
@@ -1,22 +1,27 @@
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 # Дослідити структури даних
 filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)  # Конвертуємо вміст файлу в змінну all_eq_data
 
 all_eq_dicts = all_eq_data['features']  # Створюємо список, що містить всі землетруси у вигляді словників
-print(len(all_eq_dicts))  # Перевіряємо кількість елементів: 158 словників
 
-mags, longs, lats = [], [], []  # Порожній словник для магнітуд, довгот та широт
+mags, lons, lats = [], [], []  # Порожній словник для магнітуд, довгот та широт
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']  # Видобуваємо послідовно кожну магнітуду
-    long = eq_dict['geometry']['coordinates'][0]  # Видобуваємо послідовно кожну довготу
+    lon = eq_dict['geometry']['coordinates'][0]  # Видобуваємо послідовно кожну довготу
     lat = eq_dict['geometry']['coordinates'][1]  # Видобуваємо послідовно кожну широту
     mags.append(mag)  # Додаємо значення у словник mags
-    longs.append(long)
+    lons.append(lon)
     lats.append(lat)
 
-print(mags[:10])  # Друк перших 10 значень для перевірки
-print(longs[:5])
-print(lats[:5])
+# Нанести землетруси на мапу
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='ГЛОБАЛЬНІ ЗЕМЛЕТРУСИ')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
